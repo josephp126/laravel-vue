4e8b6efaeca2691bad794848c25505025b73c48f
winner <linodeBp17@gmail.com>
2021-03-25 11:26:00 -0400

    added missing footer template

---
+++ resources/views/layouts/main-footer.blade.php
@@ -0,0 +1,112 @@
+<!-- BEGIN FOOTER ================================================== -->
+<section>
+    <div class="footer">
+        <div class="container animated fadeInUpNow notransition">
+            <div class="row">
+                <div class="col-md-3">
+                    <h1 class="footerbrand">Calypso</h1>
+                    <p>
+                        Introducing a premium HTML5 & CSS3 template for multipurpose use.
+                    </p>
+                    <p>
+                        Three awesome layouts, beautiful modern design, lots of features and skins.
+                    </p>
+                    <p>
+                        Made with &nbsp;<i class="icon-heart"></i>&nbsp; by WowThemes.net.
+                    </p>
+                </div>
+                <div class="col-md-3">
+                    <h1 class="title"><span class="colortext">F</span>ind <span class="font100">Us</span></h1>
+                    <div class="footermap">
+                        <p>
+                            <strong>Address: </strong> No.42 - 54816 Inc Calypso
+                        </p>
+                        <p>
+                            <strong>Phone: </strong> + 1 (280) 482 9537
+                        </p>
+                        <p>
+                            <strong>Fax: </strong> + 1 (372) 742 9532
+                        </p>
+                        <p>
+                            <strong>Email: </strong> wowthemesnet@gmail.com
+                        </p>
+                        <ul class="social-icons list-soc">
+                            <li><a href="#"><i class="icon-facebook"></i></a></li>
+                            <li><a href="#"><i class="icon-twitter"></i></a></li>
+                            <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                            <li><a href="#"><i class="icon-google-plus"></i></a></li>
+                            <li><a href="#"><i class="icon-skype"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <h1 class="title"><span class="colortext">C</span>lients' <span class="font100">Voice</span>
+                    </h1>
+                    <div id="quotes">
+                        <div class="textItem">
+                            <div class="avatar">
+                                <img src="http://wowthemes.net/demo/biscaya/img/demo/avatar.jpg" alt="avatar">
+                            </div>
+                            "Before turning to those moral and mental aspects of the matter which present the
+                            greatest difficulties, let the inquirer begin by mastering more elementary problems.<span
+                                style="font-family:arial;">"</span><br /><b> Jesse T, Los Angeles </b>
+                        </div>
+                        <div class="textItem">
+                            <div class="avatar">
+                                <img src="http://wowthemes.net/demo/biscaya/img/demo/avatar.jpg" alt="avatar">
+                            </div>
+                            "How often have I said to you that when you have eliminated the impossible, whatever
+                            remains, however improbable, must be the truth?<span
+                                style="font-family:arial;">"</span><br /><b>Ralph G. Flowers </b>
+                        </div>
+                    </div>
+                    <div class="clearfix">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <h1 class="title"><span class="colortext">Q</span>uick <span class="font100">Message</span>
+                    </h1>
+                    <div class="done">
+                        <div class="alert alert-success">
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+                            Your message has been sent. Thank you!
+                        </div>
+                    </div>
+                    <form method="post" action="contact.php" id="contactform">
+                        <div class="form">
+                            <input class="col-md-6" type="text" name="name" placeholder="Name">
+                            <input class="col-md-6" type="text" name="email" placeholder="E-mail">
+                            <textarea class="col-md-12" name="comment" rows="4"
+                                      placeholder="Message"></textarea>
+                            <input type="submit" id="submit" class="btn" value="Send">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p id="back-top">
+        <a href="#top"><span></span></a>
+    </p>
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <p class="pull-left">
+                        &copy; Copyright 2014 WowThemes.net
+                    </p>
+                </div>
+                <div class="col-md-8">
+                    <ul class="footermenu pull-right">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Work</a></li>
+                        <li><a href="#">Pages</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /footer section end-->
