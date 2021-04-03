4624e15ce6c4fedc017a132b5ef1b5a88ef83f41
winner <linodeBp17@gmail.com>
2021-04-03 17:46:33 -0400

    image processing, news admin management with roles, and showing the news articles on the homepage

---
+++ app/Http/Requests/NewsRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NewsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()->can('news.save');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title'       => 'required',
+            'summary'     => 'required',
+            'content'     => 'required',
+            'is_homepage' => 'boolean',
+            'file'        => 'image',
+        ];
+    }
+}
---
+++ app/Jobs/SaveImageJob.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Foundation\Bus\Dispatchable;
+use Intervention\Image\ImageManagerStatic as Imager;
+use Storage;
+
+class SaveImageJob
+{
+    use Dispatchable;
+
+    private $file;
+    private $relate;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $relate
+     * @param $file
+     */
+    public function __construct($relate, $file)
+    {
+        $this->file   = $file;
+        $this->relate = $relate;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $file   = $this->file;
+        $relate = $this->relate;
+
+        if ($file) {
+            $image = $relate->image()->create(
+                [
+                    'mime_type'   => $file->getMimeType(),
+                    'title'       => $file->getFilename(),
+                    'code_number' => 1,
+                    'hash'        => $file->hashName(),
+                ]
+            );
+
+            $newFile = Imager::make($file)->resize(
+                200,
+                null,
+                function ($c) {
+                    $c->aspectRatio();
+                }
+            )->encode();
+
+            Storage::disk('images')->put($image->hash, (string)$newFile);
+        }
+    }
+}
