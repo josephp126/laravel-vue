ea8325c7e49b488190f87926b1bc29b54bc7dcda
winner <linodeBp17@gmail.com>
2021-04-14 17:07:59 -0400

    Added new start flow and readme note

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 ## Links
 
+* https://laravelcollective.com/docs/6.x/html
 * Components - https://laravel-livewire.com/docs/2.x/
 * Permissions and roles - https://github.com/JosephSilber/bouncer
 
--- routes/admin.php
+++ routes/admin.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'admin/dashboard');
+
 Route::resource('news', NewsController::class);
+Route::put('news/{news}/star', [NewsController::class, 'star'])->name('news.star');
