b2670a8949922acd8c498e30c5c04da59b62e28b
winner <linodeBp17@gmail.com>
2021-05-11 22:47:27 +0900

    carousel work TASK-19 TASK-20 TASK-21 add jssor js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/jssor.slider-28.1.0.min.js', 'public/js')
     // .js('resources/js/calypso/bootstrap.js', 'public/js')
     // .js('resources/js/calypso/jquery.js', 'public/js')
     .js('resources/calypso-theme/js/plugins.js', 'public/js').
