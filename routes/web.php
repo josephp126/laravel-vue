29eac24941ad911821a7eb480b043008a5e6df76
winner <linodeBp17@gmail.com>
2021-05-12 15:50:13 -0400

    fixed a bug

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Auth::routes()
+Auth::routes();
 
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
 
