c784054b5ac96cb36d1845f1f84216ecf69669db
winner <linodeBp17@gmail.com>
2021-06-02 11:52:16 +0200

    slider sort

--- resources/views/admin/slide/index.blade.php
+++ resources/views/admin/slide/index.blade.php
@@ -13,13 +13,13 @@
 
 
     <div class="row">
-        @foreach ($sliders as $slide)
+        @foreach ($sliders as $index => $slide)
             <div class="col-md-3">
                 {!! Form::model($slide, ['method' => 'put', 'route' => ['admin.slider.update', $slide]]) !!}
                 <div class="d-flex flex-column justify-content-end" style="gap: 10px;">
-                    <div class="col">
+                    <div class="col m-0 p-0">
                         <img src="{{ $slide->image->url ?? '/images/broken.png' }}"
-                             style="height: 100px; margin: 0 auto;" alt="slide">
+                             style="height: 100px; width:100%; object-fit: cover; margin: 0 auto;" alt="slide">
                     </div>
                     <div class="col mb-auto">
                         <div class="form-group">
@@ -38,15 +38,28 @@
                     @include('partials.form-buttons')
                 </div>
                 {!! Form::close() !!}
-                <div class="row"
-                     style="position:absolute; top:-5px; right:10px; border-radius:10%; background:#ffffffab; padding:10px">
-                    <form class="col" action="{{ route('admin.slider.destroy', $slide) }}" method="post">
-                        @csrf
-                        @method('delete')
-                        <a rel="tooltip" data-original-title="Delete" title="Delete"
+                <div class="row" style="position:absolute; top:0px; right:10px; border-radius:12px; background:#ffffffab; padding:0px">
+                    @if($index != 0)
+                        {!! Form::model($slide, ['method' => 'put', 'route' => ['admin.slider.update', $slide], 'class' => 'col m-1 p-1']) !!}
+                            <input type="hidden" name="type" value="left">
+                            <a onclick="this.parentElement.submit()" style="cursor:pointer; color:#000">
+                                <i class="material-icons">chevron_left</i>
+                            </a>
+                        {!! Form::close() !!}
+                    @endif
+                    @if($index < count($sliders)-1)
+                        {!! Form::model($slide, ['method' => 'put', 'route' => ['admin.slider.update', $slide], 'class' => 'col m-1 p-1']) !!}
+                       <input type="hidden" name="type" value="right">
+                       <a onclick="this.parentElement.submit()" style="cursor:pointer; color:#000">
+                         <i class="material-icons">chevron_right</i>
+                       </a>
+                       {!! Form::close() !!}
+                   @endif
+                   {!! Form::model($slide, ['method' => 'delete', 'route' => ['admin.slider.destroy', $slide], 'class' => 'col m-1 p-1']) !!}
+                    <a rel="tooltip" data-original-title="Delete" title="Delete"
                            onclick="confirm('{{ __('Are you sure you want to delete this slider image?') }}') ? this.parentElement.submit() : ''"
                            style="cursor:pointer; color:#000"><i class="material-icons">close</i></a>
-                    </form>
+                    {!! Form::close() !!}
                 </div>
             </div>
         @endforeach
