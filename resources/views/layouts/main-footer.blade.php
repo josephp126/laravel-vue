875d64e7ab2859f364c24839bcd501da75e63009
winner <linodeBp17@gmail.com>
2021-06-14 20:05:37 -0400

    removed extra space under history

--- resources/views/layouts/main-footer.blade.php
+++ resources/views/layouts/main-footer.blade.php
@@ -16,7 +16,7 @@
                 <div class="col-md-3 text-white">
                     <p class="mt-2 mb-2">About</p>
                     <p class="mt-2 mb-2"><a href="{{route('about.index')}}">History</a></p>
-                    <p class="mt-3 mb-2"><a href="#">Quick Reference</a></p>
+                    <p class="mt-2 mb-2"><a href="#">Quick Reference</a></p>
                     <p class="mt-2 mb-2"><a href="#">Cross Reference</a></p>
                     <p class="mt-2 mb-2"><a href="#">Louver Selection Tool</a></p>
                     <p class="mt-2 mb-2"><a href="{{route('repfinder.index')}}">Repfinder</a></p>
