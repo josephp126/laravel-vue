2d13dc49f0de009d531736fc7bb22275891520fb
winner <linodeBp17@gmail.com>
2021-06-18 08:30:03 -0400

    save

--- resources/views/livewire/homepage-carousel.blade.php
+++ resources/views/livewire/homepage-carousel.blade.php
@@ -3,25 +3,25 @@
 
     <div class="carousel-inner">
         <div id="carousel-item-1" class="carousel-item active" style="background: url('{{url('images/homepage/carousel/slide-1-bg.svg')}}')">
-            <div class="carousel-container">
+                <div class="carousel-container">
 
-                <div class="row h-100 align-content-center">
-                    <div class="col-lg-1 left p-0"></div>
-                    <div class="col-lg-5 left p-0">
-                        <div class="top">
-                            <div class="text animated fadeInLeft">
-                                HIGH-PERFORMING LOUVERS
+                    <div class="row h-100 align-content-center">
+                        <div class="col-lg-1 left p-0"></div>
+                        <div class="col-lg-5 left p-0">
+                            <div class="top">
+                                <div class="text animated fadeInLeft">
+                                    HIGH-PERFORMING LOUVERS
+                                </div>
+                            </div>
+
+                            <div class="bottom">
+                                <img class="animated fadeInLeft perfect-for-any-condition" src="{{url('images/homepage/Air-Control-Text2.svg')}}" alt="perfect-for-any-condition"/>
                             </div>
                         </div>
-
-                        <div class="bottom">
-                            <img class="animated fadeInLeft perfect-for-any-condition" src="{{url('images/homepage/Air-Control-Text2.svg')}}" alt="perfect-for-any-condition"/>
+                        <div class="col-lg-5 right p-0">
+                            <img class="animated fadeInRight" src="{{url('images/homepage/slide-1-right-img.png')}}" alt="Slide one right image"/>
                         </div>
                     </div>
-                    <div class="col-lg-5 right p-0">
-                        <img class="animated fadeInRight" src="{{url('images/homepage/slide-1-right-img.png')}}" alt="Slide one right image"/>
-                    </div>
-                </div>
 
             </div>
         </div>
