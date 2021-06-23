5d1eb10ab44cd1bc21acc3712647950d272e1f35
winner <linodeBp17@gmail.com>
2021-06-23 06:22:21 +0400

    logo part style fix

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,11 +5,6 @@
         {!! Form::model($user, ['route' => ['admin.user.update', $user], 'method' => 'put', 'files' => true]) !!}
 
         @include('admin.users.fields')
-        <div class="row">
-            <div class="col-md-12">
-                <button class="btn btn-danger" type="submit" name="action" value="remove-logo">Remove Logo</button>
-            </div>
-        </div>
         @include('partials.form-buttons')
 
         {!! Form::close() !!}
--- resources/views/layouts/skel.blade.php
+++ resources/views/layouts/skel.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,9 +10,9 @@
 
     <title>{{ config('app.name', 'Pottorff') }}</title>
 
-@livewireStyles
+    @livewireStyles
 
-<!-- Fonts -->
+    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -23,14 +24,39 @@
 
     @livewireScripts
 </head>
+
 <body class="off">
 
-<div id="pottorffApp">
-    @yield('skel_content')
-</div>
+    <div id="pottorffApp">
+        @yield('skel_content')
+    </div>
 
-<!-- Scripts -->
-<script src="{{ mix('js/app.js') }}"></script>
-@yield('skel_scripts')
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    {{-- File Upload --}}
+    <script>
+        $(document).on('change', '.file-input', function() {
+            if (typeof(FileReader) != "undefined") {
+                var dvPreview = $("#userLogoPreview");
+                dvPreview.html("");
+                $($(this)[0].files).each(function() {
+                    var file = $(this);
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        var img = $("<img />");
+                        img.attr("class", "img-thumbnail img-xs w-100 h-100");
+                        img.attr("style", "");
+                        img.attr("src", e.target.result);
+                        dvPreview.append(img);
+                    }
+                    reader.readAsDataURL(file[0]);
+                });
+            } else {
+                alert("This browser does not support HTML5 FileReader.");
+            }
+        });
+    </script>
+    @yield('skel_scripts')
 </body>
+
 </html>
