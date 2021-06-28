5a9f140940a0623ff770ee02160963b1e2d24a76
winner <linodeBp17@gmail.com>
2021-06-28 15:36:08 -0400

    media css updates

--- resources/views/styles/carousel.blade.php
+++ resources/views/styles/carousel.blade.php
@@ -8,18 +8,18 @@
 }
 
 .carousel-homepage, .carousel-homepage, .carousel-inner {
-height: {{(int)($carousel->height_lg ?? config('carousel.heights.lg')) - 27}}px;
+    height: {{(int)($carousel->height_lg ?? config('carousel.heights.lg')) - 27}}px;
 }
 
 @media (max-width: 991.98px) {
-.carousel-homepage, .carousel-homepage, .carousel-inner {
-height: {{(int)($carousel->height_md ?? config('carousel.heights.md')) - 27}}px;
-}
+    .carousel-homepage, .carousel-homepage, .carousel-inner {
+        height: {{(int)($carousel->height_md ?? config('carousel.heights.md')) - 27}}px;
+    }
 }
 @media (max-width: 767.98px) {
-.carousel-homepage, .carousel-homepage, .carousel-inner {
-height: {{(int)($carousel->height_sm ?? config('carousel.heights.sm')) - 27}}px;
-}
+    .carousel-homepage, .carousel-homepage, .carousel-inner {
+      height: {{(int)($carousel->height_sm ?? config('carousel.heights.sm')) - 27}}px;
+    }
 }
 
 @foreach($carousel->slides as $inc => $slide)
